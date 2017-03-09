5fd964224bdf42646cc3259ba29d25c0eef77d73
Hristina Ilieva <xristina.i.ilieva@gmail.com>
2017-03-09 23:56:30 +0200

    test jenkins nunit runner

---
+++ TravelAdvisor/TravelAdvisor.UnitTests/TravelAdvisor.Web.UnitTests/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace TravelAdvisor.Web.UnitTests.Controllers
+{
+	[TestFixture]
+	public class AccountController
+	{
+		[Test]
+		public void Index()
+		{
+			// Assert
+			Assert.IsNull(null);
+		}
+	}
+}
