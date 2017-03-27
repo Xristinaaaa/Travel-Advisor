158a4f7abe75e3ff682e47acbbb9ce3279c33416
Hristina Ilieva <xristina.i.ilieva@gmail.com>
2017-03-27 08:58:19 +0300

    added unit tests

---
+++ TravelAdvisor/TravelAdvisor.UnitTests/TravelAdvisor.Web.UnitTests/Admin/Controllers/TripsControllerUnitTests/Constructor_Should.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+using TravelAdvisor.Business.Services.Data.Contracts;
+using TravelAdvisor.Business.Services.Logic.Contracts;
+using TravelAdvisor.Web.Areas.Admin.Controllers;
+
+namespace TravelAdvisor.Web.UnitTests.Admin.Controllers.TripsControllerUnitTests
+{
+	[TestFixture]
+	public class Constructor_Should
+	{
+		[Test]
+		public void CreateTripsController_WhenParamsAreValid()
+		{
+			// Arrange
+			var mockedIDestinationService = new Mock<IDestinationService>();
+			var mockedTripService = new Mock<ITripService>();
+			var mockedMappingService = new Mock<IMappingService> ();
+			var mockedImageService = new Mock<IImageService>();
+			var tripsController = new TripsController(mockedTripService.Object, mockedIDestinationService.Object,
+				mockedMappingService.Object, mockedImageService.Object);
+
+			//Act & Assert
+			Assert.That(tripsController, Is.InstanceOf<TripsController>());
+		}
+
+		[Test]
+		public void ThrowNullException_WhenDestinationServiceIsNull()
+		{
+			//Arrange
+			Mock<IDestinationService> mockedIDestinationService = null;
+			var mockedTripService = new Mock<ITripService>();
+			var mockedMappingService = new Mock<IMappingService>();
+			var mockedImageService = new Mock<IImageService>();
+
+			//Act & Assert
+			Assert.Throws<NullReferenceException>(() => new TripsController(mockedTripService.Object, mockedIDestinationService.Object,
+				mockedMappingService.Object, mockedImageService.Object));
+		}
+
+		[Test]
+		public void ThrowNullException_WhenTripServiceIsNull()
+		{
+			//Arrange
+			var mockedIDestinationService = new Mock<IDestinationService>();
+			Mock<ITripService> mockedTripService = null;
+			var mockedMappingService = new Mock<IMappingService>();
+			var mockedImageService = new Mock<IImageService>();
+
+			//Act & Assert
+			Assert.Throws<NullReferenceException>(() => new TripsController(mockedTripService.Object, mockedIDestinationService.Object,
+				mockedMappingService.Object, mockedImageService.Object));
+		}
+
+		[Test]
+		public void ThrowNullException_WhenMappingServiceIsNull()
+		{
+			//Arrange
+			var mockedIDestinationService = new Mock<IDestinationService>();
+			var mockedTripService = new Mock<ITripService>();
+			Mock<IMappingService> mockedMappingService = null;
+			var mockedImageService = new Mock<IImageService>();
+
+			//Act & Assert
+			Assert.Throws<NullReferenceException>(() => new TripsController(mockedTripService.Object, mockedIDestinationService.Object,
+				mockedMappingService.Object, mockedImageService.Object));
+		}
+
+		[Test]
+		public void ThrowNullException_WhenImageServiceIsNull()
+		{
+			//Arrange
+			var mockedIDestinationService = new Mock<IDestinationService>();
+			var mockedTripService = new Mock<ITripService>();
+			var mockedMappingService = new Mock<IMappingService>();
+			Mock<IImageService> mockedImageService = null;
+
+			//Act & Assert
+			Assert.Throws<NullReferenceException>(() => new TripsController(mockedTripService.Object, mockedIDestinationService.Object,
+				mockedMappingService.Object, mockedImageService.Object));
+		}
+	}
+}
---
+++ TravelAdvisor/TravelAdvisor.UnitTests/TravelAdvisor.Web.UnitTests/Admin/Controllers/TripsControllerUnitTests/Index_Should.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Moq;
+using NUnit.Framework;
+using TestStack.FluentMVCTesting;
+using TravelAdvisor.Business.Models.Trips;
+using TravelAdvisor.Business.Services.Data.Contracts;
+using TravelAdvisor.Business.Services.Logic.Contracts;
+using TravelAdvisor.Web.Areas.Admin.Controllers;
+using TravelAdvisor.Web.Areas.Admin.Models.Trips;
+
+namespace TravelAdvisor.Web.UnitTests.Admin.Controllers.TripsControllerUnitTests
+{
+	[TestFixture]
+	public class Index_Should
+	{
+		[Test]
+		public void ReturnDefaultView()
+		{
+			// Arrange
+			var mockedIDestinationService = new Mock<IDestinationService>();
+			var mockedTripService = new Mock<ITripService>();
+			var mockedMappingService = new Mock<IMappingService>();
+			var mockedImageService = new Mock<IImageService>();
+			var tripsController = new TripsController(mockedTripService.Object, mockedIDestinationService.Object,
+				mockedMappingService.Object, mockedImageService.Object);
+
+			//Act & Assert
+			tripsController
+				.WithCallTo(c => c.Index())
+				.ShouldRenderDefaultView();
+		}
+
+		[Test]
+		public void ReturnDefaultViewWithViewbagOfCountries()
+		{
+			// Arrange
+			var mockedIDestinationService = new Mock<IDestinationService>();
+			var mockedTripService = new Mock<ITripService>();
+			var mockedMappingService = new Mock<IMappingService>();
+			var mockedImageService = new Mock<IImageService>();
+			var tripsController = new TripsController(mockedTripService.Object, mockedIDestinationService.Object,
+				mockedMappingService.Object, mockedImageService.Object);
+
+
+			var countries = new List<string>();
+
+			// Act 
+			tripsController
+				.WithCallTo(c => c.Create())
+				.ShouldRenderDefaultView();
+
+			//Assert
+			Assert.AreEqual(countries, tripsController.ViewBag.Destinations);
+		}
+	}
+}
